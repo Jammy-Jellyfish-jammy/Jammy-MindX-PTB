f3bde1849abed9387e45127e9cb7922c761f0379
Jammy Jellyfish <trannguyenminhkhang0000@gmail.com>
2025-08-31 19:43:10 +0700

    upload 7.

---
+++ PTB_Homework/homework7.1.py
@@ -0,0 +1,14 @@
+#Homework 6.2 - Tim so chan
+#created date: 21-8-2025
+
+getInputNum = int(input("n? "))
+
+outputNum = 0
+sum = 0
+
+while sum <= getInputNum:
+    outputNum+=1
+    sum = sum + outputNum
+    print(outputNum, end = " ")
+
+print()
